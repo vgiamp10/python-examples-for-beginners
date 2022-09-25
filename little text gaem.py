696d1538ab4bdb670a51a6615d289a9612eb9ce6
vgiamp10 <84873533+vgiamp10@users.noreply.github.com>
2022-09-25 11:27:08 +0200

    new things

---
+++ little text gaem.py
@@ -0,0 +1,4 @@
+print ("Hello and welcome to [replace this text with the name of the game]")
+nickname = input("please, insert your name here: ")
+print (nickname , ", welcome to [insert name of the game again]")
+#continue the rest urself im tired lol
